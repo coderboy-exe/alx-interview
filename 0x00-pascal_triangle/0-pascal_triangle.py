a551588589c77fd5bc6a3683662969506f6bd6b4
Uchechukwu Ottah <usottah@gmail.com>
2023-03-23 11:19:43 +0000

    Pycodestyle linting

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -7,17 +7,17 @@ def pascal_triangle(n):
 
     if n <= 0:
         return []
-    
-    triangle=[1]
-    result=[triangle]
+
+    triangle = [1]
+    result = [triangle]
 
     for i in range(n-1):
-        row=[]
+        row = []
         row.append(triangle[0])
-        for i in range(len(triangle)-1):
-            row.append(triangle[i]+triangle[i+1])
+        for i in range(len(triangle) - 1):
+            row.append(triangle[i] + triangle[i + 1])
         row.append(triangle[-1])
-        triangle=row
+        triangle = row
         result.append(row)
 
     return result
