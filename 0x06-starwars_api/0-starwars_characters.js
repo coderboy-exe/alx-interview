0943f3801ca21c55b356cf5f7a54e54579e23d94
Uchechukwu Ottah <usottah@gmail.com>
2023-05-04 02:20:35 -0700

    Get movie characters from Starwars API (in order)

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,41 @@
+#!/usr/bin/node
+const request = require('request');
+
+const filmId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${filmId}/`;
+
+const getCharacters = () => {
+  return new Promise((resolve, reject) => {
+    request(url, (err, res, body) => {
+      if (err) {
+        console.error('error:', err);
+        reject(err);
+      } else {
+        const response = JSON.parse(body);
+        const characters = response.characters;
+        resolve(characters);
+      }
+    });
+  });
+};
+
+const getNames = async () => {
+  const characters = await getCharacters(filmId);
+
+  for (const charUrl of characters) {
+    const character = await new Promise((resolve, reject) => {
+      request(charUrl, (err, res, body) => {
+        if (err) {
+          console.error('error:', err);
+          reject(err);
+        } else {
+          const response = JSON.parse(body);
+          resolve(response);
+        }
+      });
+    });
+    console.log(character.name);
+  }
+};
+
+getNames();
