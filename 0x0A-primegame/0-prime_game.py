1276d6a42980b4844e629f2b21e0138ddad85319
Uchechukwu Ottah <usottah@gmail.com>
2023-06-04 22:28:13 +0000

    Update

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -2,21 +2,13 @@
 """ Prime game algo """
 
 
-#def isWinner(x, nums):
-    #arr = []
-
-    #nimber = 0  # or grundy number  
-    #for i in nums:
-    #    nimber ^= nums[i % len(nums)];
-
-   # return "Ben" if nimber > 0 else "Maria"
-
 def calculate_highest_power_of_two(n):
     """ Calculates the highest power of 2 that divides a number n """
     power_of_two = n
     while power_of_two & (power_of_two - 1) != 0:
         power_of_two = power_of_two & (power_of_two - 1)
     return power_of_two
+
 
 def isWinner(x, nums):
     """ Determines the winner of the game """
@@ -30,4 +22,3 @@ def isWinner(x, nums):
             nimber ^= highest_power_of_two
 
     return "Ben" if nimber > 0 else "Maria"
-
