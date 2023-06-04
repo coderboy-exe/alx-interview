61dc9d9022ca5d52873dc2d4da156e05f800c997
Uchechukwu Ottah <usottah@gmail.com>
2023-06-04 22:23:58 +0000

    documentation

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -12,12 +12,14 @@
    # return "Ben" if nimber > 0 else "Maria"
 
 def calculate_highest_power_of_two(n):
+    """ Calculates the highest power of 2 that divides a number n """
     power_of_two = n
     while power_of_two & (power_of_two - 1) != 0:
         power_of_two = power_of_two & (power_of_two - 1)
     return power_of_two
 
 def isWinner(x, nums):
+    """ Determines the winner of the game """
     nimber = 0  # or grundy number
 
     for n in nums:
